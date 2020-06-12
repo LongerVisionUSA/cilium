LongerVisionUSA/cilium

commit d613dea5d52494da703e72ef0011e2849f7f19ce

install/kubernetes: use HTTP for agent {liveness,readiness}Probe
Use the newly introduced /healthz HTTP path exposed on localhost to
check for liveness/readiness of the Cilium agent.

In the long term this would also allow to exclude the cilium CLI tool
from the runtime image and move it into a separate image used with an
ephemeral container for debugging.

Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

install/kubernetes/cilium/charts/agent/templates/daemonset.yaml
@@ -55,11 +55,11 @@ spec:
         command:
         - cilium-agent
         livenessProbe:
-          exec:
-            command:
-            - cilium
-            - status
-            - --brief
+          httpGet:
+            host: '127.0.0.1'
+            path: /healthz
+            port: {{ .Values.global.agent.healthPort }}
+            scheme: HTTP
           failureThreshold: 10
           # The initial delay for the liveness probe is intentionally large to
           # avoid an endless kill & restart cycle if in the event that the initial
@@ -69,11 +69,11 @@ spec:
           successThreshold: 1
           timeoutSeconds: 5
         readinessProbe:
-          exec:
-            command:
-            - cilium
-            - status
-            - --brief
+          httpGet:
+            host: '127.0.0.1'
+            path: /healthz
+            port: {{ .Values.global.agent.healthPort }}
+            scheme: HTTP
           failureThreshold: 3
           initialDelaySeconds: 5
           periodSeconds: 30

daemon/cmd/agenthealth.go
@@ -52,7 +52,7 @@ func (d *Daemon) startAgentHealthHTTPService(addr string) {
 	lc := net.ListenConfig{Control: setsockoptReuseAddrAndPort}
 	ln, err := lc.Listen(context.Background(), "tcp", addr)
 	if err != nil {
-		log.Fatal(err)
+		log.WithError(err).Fatalf("Unable to listen on %s for healthz status API server", addr)
 	}
 
 	mux := http.NewServeMux()
@@ -82,7 +82,7 @@ func (d *Daemon) startAgentHealthHTTPService(addr string) {
 		if err == http.ErrServerClosed {
 			log.Info("healthz status API server shutdown")
 		} else if err != nil {
-			log.WithError(err).Fatal("Unable to start status healthz status API server")
+			log.WithError(err).Fatal("Unable to start healthz status API server")
 		}
 	}()
 	log.Infof("Started healthz status API server on address %s", addr)

install/kubernetes/quick-install.yaml
@@ -364,11 +364,11 @@ spec:
         command:
         - cilium-agent
         livenessProbe:
-          exec:
-            command:
-            - cilium
-            - status
-            - --brief
+          httpGet:
+            host: '127.0.0.1'
+            path: /healthz
+            port: 9876
+            scheme: HTTP
           failureThreshold: 10
           # The initial delay for the liveness probe is intentionally large to
           # avoid an endless kill & restart cycle if in the event that the initial
@@ -378,11 +378,11 @@ spec:
           successThreshold: 1
           timeoutSeconds: 5
         readinessProbe:
-          exec:
-            command:
-            - cilium
-            - status
-            - --brief
+          httpGet:
+            host: '127.0.0.1'
+            path: /healthz
+            port: 9876
+            scheme: HTTP
           failureThreshold: 3
           initialDelaySeconds: 5
           periodSeconds: 30

commit 5ef27ccded0aac21e75aef1517d7c13597cfee36

test/k8s: keep configmap across upgrade test
Since we have new options set for new Cilium installations those
might brake the connectivity when doing upgrades.

To ideally test an upgrade, we should keep the existing ConfigMap
from the previous the version.

This also fixes the upgrade guide using helm as using `--set
config.enabled=false` deploys Cilium without its configuration map,
deleting the existing one from the cluster.

Also fixes the Cilium DaemonSet since its upgrade does not work using
kubectl apply given that we have introduced a new liviness and readiness
probes in the DaemonSet. Added upgrade notes to keep the old probes
while users perform the upgrades.

Fixes: d613dea5d524 ("install/kubernetes: use HTTP for agent {liveness,readiness}Probe")
Signed-off-by: André Martins <andre@cilium.io>

install/kubernetes/cilium/charts/agent/templates/daemonset.yaml
@@ -55,6 +55,13 @@ spec:
         command:
         - cilium-agent
         livenessProbe:
+{{- if .Values.keepDeprecatedProbes }}
+          exec:
+            command:
+            - cilium
+            - status
+            - --brief
+{{- else }}
           httpGet:
             host: '127.0.0.1'
             path: /healthz
@@ -63,6 +70,7 @@ spec:
             httpHeaders:
             - name: "brief"
               value: "true"
+{{- end }}
           failureThreshold: 10
           # The initial delay for the liveness probe is intentionally large to
           # avoid an endless kill & restart cycle if in the event that the initial
@@ -72,6 +80,13 @@ spec:
           successThreshold: 1
           timeoutSeconds: 5
         readinessProbe:
+{{- if .Values.keepDeprecatedProbes }}
+          exec:
+            command:
+            - cilium
+            - status
+            - --brief
+{{- else }}
           httpGet:
             host: '127.0.0.1'
             path: /healthz
@@ -80,6 +95,7 @@ spec:
             httpHeaders:
             - name: "brief"
               value: "true"
+{{- end }}
           failureThreshold: 3
           initialDelaySeconds: 5
           periodSeconds: 30

install/kubernetes/cilium/values.yaml
@@ -7,6 +7,8 @@ agent:
   sleepAfterInit: false
   # Keep the deprecated selector labels when deploying Cilium DaemonSet
   keepDeprecatedLabels: false
+  # Keep the deprecated probes when deploying Cilium DaemonSet
+  keepDeprecatedProbes: false
 
 # Include the cilium-config ConfigMap
 config:

test/helpers/kubectl.go
@@ -2355,6 +2355,24 @@ func (kub *Kubectl) RunHelm(action, repo, helmName, version, namespace string, o
 		"%s", action, helmName, repo, version, namespace, optionsString)), nil
 }
 
+// RunHelmTemplate runs the helm template command for a specific version.
+func (kub *Kubectl) RunHelmTemplateApply(repo, helmName, version, namespace string, options map[string]string) (*CmdRes, error) {
+	err := kub.overwriteHelmOptions(options)
+	if err != nil {
+		return nil, err
+	}
+	optionsString := ""
+
+	for k, v := range options {
+		optionsString += fmt.Sprintf(" --set %s=%s ", k, v)
+	}
+
+	return kub.ExecMiddle(fmt.Sprintf("helm template %s %s "+
+		"--version=%s "+
+		"--namespace=%s "+
+		"%s | %s apply -f -", helmName, repo, version, namespace, optionsString, KubectlCmd)), nil
+}
+
 // CiliumUninstall uninstalls Cilium with the provided Helm options.
 func (kub *Kubectl) CiliumUninstall(filename string, options map[string]string) error {
 	return kub.ciliumUninstallHelm(filename, options)

test/k8sT/Updates.go
@@ -445,16 +445,26 @@ func InstallAndValidateCiliumUpgrades(kubectl *helpers.Kubectl, oldHelmChartVers
 		By("Upgrading Cilium to %s", newHelmChartVersion)
 		opts = map[string]string{
 			"global.tag": newImageVersion,
+			// Do not use new configuration map as we are testing an upgrade
+			// scenario where the user deploys Cilium while keeping its existing
+			// configuration map.
+			"config.enabled": "false",
 		}
 		// We have removed the labels since >= 1.7 and we are only testing
 		// starting from 1.6.
 		if oldHelmChartVersion == "1.6-dev" {
 			opts["agent.keepDeprecatedLabels"] = "true"
 		}
+		// We have replaced the liveliness and readiness probes since >= 1.8 and
+		// we need to keep those deprecated probes from <1.8-dev to >=1.8
+		// upgrades since kubernetes does not do `kubectl apply -f` correctly.
+		switch oldHelmChartVersion {
+		case "1.6-dev", "1.7-dev":
+			opts["agent.keepDeprecatedProbes"] = "true"
+		}
 
 		EventuallyWithOffset(1, func() (*helpers.CmdRes, error) {
-			return kubectl.RunHelm(
-				"upgrade",
+			return kubectl.RunHelmTemplateApply(
 				filepath.Join(kubectl.BasePath(), helpers.HelmTemplate),
 				"cilium",
 				newHelmChartVersion,
@@ -481,9 +491,11 @@ func InstallAndValidateCiliumUpgrades(kubectl *helpers.Kubectl, oldHelmChartVers
 		checkNoInteruptsInSVCFlows()
 
 		By("Downgrading cilium to %s image", oldHelmChartVersion)
-		// rollback cilium 1 because it's the version that we have started
-		// cilium with in this updates test.
-		cmd = kubectl.ExecMiddle("helm rollback cilium 1 --namespace=" + helpers.CiliumNamespace)
+		// Install the previous configuration using helm. This is a hack
+		// as we have previously upgrade Cilium using kubectl apply -f.
+		// helm get keeps the values used when we installed Cilium wihtout the
+		// upgrade changes that we have done with kubectl apply -f.
+		cmd = kubectl.ExecMiddle(fmt.Sprintf("helm get -n %s manifest cilium | kubectl apply -f -", helpers.CiliumNamespace))
 		ExpectWithOffset(1, cmd).To(helpers.CMDSuccess(), "Cilium %q was not able to be deployed", oldHelmChartVersion)
 
 		err = helpers.WithTimeout(